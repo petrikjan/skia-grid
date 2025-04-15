84f997cc7931624685074242e675c7ff88e1a4ac
Jan Petrik <petrikjan@centrum.cz>
2025-04-15 16:42:11 +0200

    simpler version

--- App.tsx
+++ App.tsx
@@ -26,7 +26,6 @@ import {
   Line,
   Rect,
   rect,
-  Paragraph,
   TextAlign,
   Skia,
   useFonts,
@@ -63,14 +62,8 @@ const getScrollPositionForCurrentTime = screenWidth => {
   const currentMinutes = now.getHours() * 60 + now.getMinutes();
   const currentPositionInPixels = (currentMinutes / 60) * TIME_SLOT_WIDTH;
   const scrollPosition =
-    currentPositionInPixels - screenWidth / 2 + styles.channelsWrapper.width;
+    currentPositionInPixels - screenWidth / 2 + CHANNEL_NAME_WIDTH;
   return scrollPosition;
-};
-
-const formatTime = (date: string) => {
-  const hours = new Date(date).getHours().toString().padStart(2, '0');
-  const minutes = new Date(date).getMinutes().toString().padStart(2, '0');
-  return `${hours}:${minutes}`;
 };
 
 const calculateProgramLength = (program: any) => {
@@ -98,101 +91,21 @@ const generateTimeline = () => {
   return timeline;
 };
 
-const calculateCurrentTimeOffset = () => {
-  const now = new Date();
-  if (now < DAY_START) {
-    return 0;
-  }
-  if (now > DAY_END) {
-    return ((24 * 60) / 60) * TIME_SLOT_WIDTH;
-  }
-  const minutesSinceStartOfDay = (now - DAY_START) / (1000 * 60);
-  return (
-    (minutesSinceStartOfDay / 60) * TIME_SLOT_WIDTH + styles.channelName.width
-  );
-};
-
-const isProgramRunning = (program: any) => {
-  const now = new Date();
-  return now >= program.start && now <= program.end;
-};
-
 const CEL = memo(
-  ({program, fontMgr}) => {
+  ({program}) => {
     const rctMargin = rect(
       program.x,
       program.y,
       program.width - 10,
       ROW_HEIGHT,
     );
-    const paragraphStyle = {
-      textAlign: TextAlign.Left,
-      maxLines: 1,
-    };
-    const textStyleTitle = {
-      color: Skia.Color(styles.programTextTitle.color),
-      fontFamilies: [styles.programText.fontFamily],
-      fontSize: styles.programTextTitle.fontSize,
-    };
-    const textStyle = {
-      color: Skia.Color(styles.programTextTitle.color),
-      fontFamilies: [styles.programText.fontFamily],
-      fontSize: styles.programText.fontSize,
-    };
-    const paragraphTitle = Skia.ParagraphBuilder.Make(paragraphStyle, fontMgr)
-      .pushStyle(textStyleTitle)
-      .addText(program.programName)
-      .pop()
-      .build();
-    const paragraph = Skia.ParagraphBuilder.Make(paragraphStyle, fontMgr)
-      .pushStyle(textStyle)
-      .addText(
-        `${formatTime(program.start)} - ${formatTime(program.end)} | ${
-          program.genre
-        }`,
-      )
-      .pop()
-      .build();
 
-    paragraphTitle.layout(program.width - 10);
     return (
       <>
-        {program.running && (
-          <Rect
-            x={program.x}
-            y={program.y}
-            width={program.width}
-            height={ROW_HEIGHT}
-            color="rgba(255, 255, 255, 0.1)"
-          />
-        )}
-        {program.selected && (
-          <Rect
-            x={program.x}
-            y={program.y}
-            width={program.width}
-            height={ROW_HEIGHT}
-            color={'white'}
-          />
-        )}
         {program.width > 25 && (
           <Group clip={rctMargin} key={`programCell${program.programId}`}>
-            <Group layer={<Paint opacity={program.isAgeBlocked ? 0.5 : 1} />}>
-              <Paragraph
-                paragraph={paragraphTitle}
-                x={program.x + 10}
-                y={8 + program.y}
-                width={program.width - 10}
-              />
-            </Group>
-            <Group layer={<Paint opacity={0.5} />}>
-              <Paragraph
-                paragraph={paragraph}
-                x={program.x + 10}
-                y={32 + program.y}
-                width={program.width - 10}
-              />
-            </Group>
+            <Group layer={<Paint opacity={program.isAgeBlocked ? 0.5 : 1} />} />
+            <Group layer={<Paint opacity={0.5} />} />
           </Group>
         )}
       </>
@@ -206,19 +119,16 @@ const CEL = memo(
   },
 );
 const ROW = memo(
-  ({row, fontMgr}) => {
+  ({row}) => {
     'use forget';
 
-    if (!fontMgr) {
-      return null;
-    }
     return row.programs.map((program, index) => (
       <Group key={`row${program.programId}`}>
-        <CEL program={program} fontMgr={fontMgr} />
+        <CEL program={program} />
         <Line
           p1={vec(program.x + program.width, program.y)}
           p2={vec(program.x + program.width, program.y + ROW_HEIGHT)}
-          color={styles.program.borderColor}
+          color={'rgba(255,255,255,0.2)'}
           style="stroke"
           strokeWidth={1}
         />
@@ -226,7 +136,7 @@ const ROW = memo(
           <Line
             p1={vec(program.x, program.y)}
             p2={vec(program.x, program.y + ROW_HEIGHT)}
-            color={styles.program.borderColor}
+            color={'rgba(255,255,255,0.2)'}
             style="stroke"
             strokeWidth={1}
           />
@@ -235,10 +145,7 @@ const ROW = memo(
     ));
   },
   (prevProps, nextProps) => {
-    if (
-      _.isEqual(prevProps.row, nextProps.row) &&
-      prevProps.currentTimeOffset === nextProps.currentTimeOffset
-    ) {
+    if (_.isEqual(prevProps.row, nextProps.row)) {
       return true;
     }
     return false;
@@ -273,9 +180,6 @@ const EPG = memo(
   () => {
     'use forget';
     const timeline = useMemo(() => generateTimeline(), []);
-    const [initScroll, setInitScroll] = useState<false | number>(false);
-    const [canSetCurrentTimeLine, setCanSetCurrentTimeLine] =
-      useState<boolean>(true);
     const [size, onLayout] = useComponentSize();
     const [scroll, setScroll] = useState([0, 0]);
     const w = useSharedValue(size?.width || 0);
@@ -286,7 +190,6 @@ const EPG = memo(
     const scrollRef = useRef([0, 0]);
     const processing = useRef(false);
     const formattedDate = '2025-04-09';
-    const todayFormattedDate = '2025-04-09';
     const dayStart = useMemo(
       () =>
         new Date(
@@ -297,14 +200,13 @@ const EPG = memo(
           0,
           0,
         ),
-      [selectedDate],
+      [],
     );
-    const [currentTimeOffset, setCurrentTimeOffset] = useState(
-      calculateCurrentTimeOffset(),
-    );
+
     const {channels} = useMemo(() => {
       return epg[formattedDate] || {channels: []};
     }, [epg, formattedDate]);
+
     const lastRenderTime = useRef(performance.now());
 
     useEffect(() => {
@@ -312,19 +214,15 @@ const EPG = memo(
       lastRenderTime.current = now;
     });
 
-    const fontMgr = useFonts({
-      'TeleGroteskNext-Medium': [TeleGroteskNextRegular],
-      'TeleGroteskNext-Regular': [TeleGroteskNextRegular],
-    });
     const translateX = useSharedValue(0);
     const translateY = useSharedValue(0);
     const prevTranslateX = useSharedValue(0);
     const prevTranslateY = useSharedValue(0);
 
     const totalContentWidth =
-      timeline.length * TIME_SLOT_WIDTH + styles.channelName.width;
+      timeline.length * TIME_SLOT_WIDTH + CHANNEL_NAME_WIDTH;
     const totalContentHeight =
-      channelsBrief.array.length * ROW_HEIGHT + styles.timelineRow.height;
+      channelsBrief.array.length * ROW_HEIGHT + ROW_HEIGHT;
 
     const setScrollThrottled = useCallback(
       _.throttle((_x, offset, _y) => {
@@ -351,18 +249,7 @@ const EPG = memo(
       } else {
         processing.current = false;
       }
-    }, [scroll]);
-
-    useEffect(() => {
-      const interval = setInterval(() => {
-        if (!processing.current) {
-          setCurrentTimeOffset(calculateCurrentTimeOffset(new Date()));
-        }
-      }, 10000);
-      return () => {
-        clearInterval(interval);
-      };
-    }, []);
+    }, [scroll, setScrollThrottled]);
 
     useEffect(() => {
       w.set(size?.width || 0);
@@ -370,7 +257,7 @@ const EPG = memo(
     }, [size]);
 
     useEffect(() => {
-      if (size && canSetCurrentTimeLine) {
+      if (size) {
         const position = Math.min(
           getScrollPositionForCurrentTime(size.width) < 0
             ? 0
@@ -378,13 +265,13 @@ const EPG = memo(
           totalContentWidth - size.width,
         );
 
-        setInitScroll(position);
         translateX.set(-position);
         scrollRef.current = [position, 0];
         setScrollThrottled([position, 0]);
       }
-    }, [size, canSetCurrentTimeLine]);
+    }, [size]);
 
+    // try to comment this out, then the recycling of components and new programs is not computing
     useAnimatedReaction(
       () => [
         translateX.value,
@@ -444,29 +331,6 @@ const EPG = memo(
         );
       });
 
-    const handleClick = useMemo(
-      () => (clickX, clickY) => {
-        setCanSetCurrentTimeLine(false);
-        const rowNumber = Math.floor(clickY / ROW_HEIGHT) - 1;
-
-        cuttedEpgRef.current[rowNumber]?.some(program => {
-          if (program.x < clickX && program.x + program.width > clickX) {
-            return true;
-          }
-        });
-      },
-      [],
-    );
-
-    const tapGesture = Gesture.Tap().onEnd(event => {
-      runOnJS(handleClick)(
-        event.x + translateX.value * -1,
-        event.y + translateY.value * -1,
-      );
-    });
-
-    const combinedGesture = Gesture.Exclusive(panGesture, tapGesture);
-
     useEffect(() => {
       const from = 0;
       const to = channelsBrief.array.length;
@@ -494,7 +358,7 @@ const EPG = memo(
               const programEndX =
                 programStartX + (programLength * TIME_SLOT_WIDTH) / 60;
 
-              const x = programStartX + styles.channelsWrapper.width;
+              const x = programStartX + CHANNEL_NAME_WIDTH;
               const y = i * ROW_HEIGHT + ROW_HEIGHT;
               const width = programEndX - programStartX;
               const key = `${ch.channelId}_${ch.items[n].id}`;
@@ -519,7 +383,6 @@ const EPG = memo(
                   y,
                   width,
                   height: ROW_HEIGHT,
-                  running: isProgramRunning(ch.items[n]),
                   genre: ch.items[n].genre,
                 });
               }
@@ -537,7 +400,7 @@ const EPG = memo(
       return () => {
         Object.keys(out).forEach(k => delete out[k]);
       };
-    }, [currentTimeOffset, formattedDate, channels, epgMap, channelsBrief]);
+    }, [formattedDate, channels, epgMap, channelsBrief]);
 
     useEffect(() => {
       const NUM_ROWS = Math.ceil((size?.height || 0) / ROW_HEIGHT) + 1;
@@ -550,40 +413,6 @@ const EPG = memo(
       }
       setRecycled(recycedRows);
     }, [size]);
-
-    const Times = useMemo(
-      () =>
-        timeline.map((time, index) => {
-          if (!fontMgr) {
-            return null;
-          }
-
-          const paragraphStyle = {
-            textAlign: TextAlign.Center,
-          };
-          const textStyle = {
-            color: Skia.Color('white'),
-            fontFamilies: ['TeleGroteskNext-Medium'],
-            fontSize: 20,
-          };
-          const paragraph = Skia.ParagraphBuilder.Make(paragraphStyle, fontMgr)
-            .pushStyle(textStyle)
-            .addText(time)
-            .pop()
-            .build();
-
-          return (
-            <Paragraph
-              key={time}
-              paragraph={paragraph}
-              x={index * TIME_SLOT_WIDTH + CHANNEL_NAME_WIDTH}
-              y={18}
-              width={TIME_SLOT_WIDTH}
-            />
-          );
-        }),
-      [fontMgr],
-    );
 
     const GRID = useMemo(() => {
       if (!size) {
@@ -629,13 +458,11 @@ const EPG = memo(
           totalContentWidth={totalContentWidth}
           key={row.key}
           program={row.program}
-          currentTimeOffset={currentTimeOffset}
-          fontMgr={fontMgr}
         />
       ));
       updatedArray.length = 0;
       return c;
-    }, [cuttedEpg, recycled, scroll, size, currentTimeOffset, fontMgr]);
+    }, [cuttedEpg, recycled, scroll, size]);
 
     const RowLines = useMemo(
       () =>
@@ -645,7 +472,7 @@ const EPG = memo(
               key={`line_${channel.channelId}`}
               p1={vec(0, (index + 1) * ROW_HEIGHT)}
               p2={vec(totalContentWidth, (index + 1) * ROW_HEIGHT)}
-              color={styles.program.borderColor}
+              color={'rgba(255,255,255,0.2)'}
               style="stroke"
               strokeWidth={1}
             />
@@ -659,17 +486,15 @@ const EPG = memo(
       {translateY: translateY.value},
     ]);
 
-    const transformX = useDerivedValue(() => [{translateX: translateX.value}]);
-
     return (
       <GestureHandlerRootView>
         <View style={{flex: 1, backgroundColor: 'black'}} onLayout={onLayout}>
           <StatusBar hidden />
 
-          {size && initScroll !== false && (
+          {size !== null && (
             <View
               style={{
-                ...styles.container,
+                overflow: 'hidden',
                 width: size.width,
                 height: size.height,
               }}>
@@ -684,47 +509,8 @@ const EPG = memo(
                   }}>
                   <Group transform={transform}>{GRID}</Group>
                   <Group transform={transform}>{RowLines}</Group>
-                  {formattedDate === todayFormattedDate && (
-                    <Group transform={transformX}>
-                      <Line
-                        p1={vec(currentTimeOffset, ROW_HEIGHT / 2 / 2)}
-                        p2={vec(currentTimeOffset, size.height)}
-                        color={styles.currentTimeLine.backgroundColor}
-                        style="stroke"
-                        strokeWidth={2}
-                      />
-                    </Group>
-                  )}
-                  <Rect
-                    x={0}
-                    y={0}
-                    width={size.width}
-                    height={ROW_HEIGHT}
-                    color={styles.timelineRow.backgroundColor}
-                  />
-                  <Group transform={transformX}>
-                    {Times}
-                    {formattedDate === todayFormattedDate && (
-                      <>
-                        <Line
-                          p1={vec(currentTimeOffset, ROW_HEIGHT / 2 / 2)}
-                          p2={vec(currentTimeOffset, ROW_HEIGHT)}
-                          color={styles.currentTimeLine.backgroundColor}
-                          style="stroke"
-                          strokeWidth={2}
-                        />
-                      </>
-                    )}
-                  </Group>
-                  <Line
-                    p1={vec(0, ROW_HEIGHT)}
-                    p2={vec(totalContentWidth, ROW_HEIGHT)}
-                    color={styles.timelineRow.borderBottomColor}
-                    style="stroke"
-                    strokeWidth={1}
-                  />
                 </Canvas>
-                <GestureDetector gesture={combinedGesture}>
+                <GestureDetector gesture={panGesture}>
                   <View
                     style={{
                       flexDirection: 'row',
@@ -752,147 +538,5 @@ const EPG = memo(
     return false;
   },
 );
-
-const styles = StyleSheet.create({
-  container: {
-    overflow: 'hidden',
-  },
-  animatedContainer: {
-    flexDirection: 'column',
-  },
-  timelineRowWeb: {
-    top: 0,
-    zIndex: 3,
-    position: 'absolute',
-  },
-  timelineRow: {
-    position: 'absolute',
-    height: ROW_HEIGHT,
-    flexDirection: 'row',
-    backgroundColor: 'grey',
-    borderBottomWidth: 1,
-    borderStyle: 'solid',
-    borderBottomColor: 'rgba(255,255,255,0.2)',
-    paddingLeft: CHANNEL_NAME_WIDTH,
-  },
-  channelsWrapper: {
-    top: ROW_HEIGHT,
-    width: CHANNEL_NAME_WIDTH,
-    position: 'absolute',
-    overflow: 'hidden',
-    backgroundColor: 'rgba(35,35,35, 0.9)',
-  },
-  channelsContainer: {
-    position: 'absolute',
-    flexDirection: 'column',
-    width: CHANNEL_NAME_WIDTH,
-  },
-  currentTimeLine: {
-    position: 'absolute',
-    flexDirection: 'column',
-    width: 3,
-    backgroundColor: 'white',
-    top: ROW_HEIGHT / 2,
-  },
-  currentTimeLineLabel: {
-    position: 'absolute',
-    flexDirection: 'column',
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 2,
-    height: ROW_HEIGHT / 2,
-    width: 60,
-    top: ROW_HEIGHT / 2 / 2,
-  },
-  label: {
-    backgroundColor: 'white',
-    borderRadius: 2,
-    height: '50%',
-    width: 60,
-  },
-  labelText: {
-    color: 'white',
-    fontFamily: 'TeleGroteskNext-Regular',
-    fontSize: 18,
-    fontWeight: '400',
-    textAlign: 'center',
-    width: '100%',
-  },
-  timeSlot: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  timelineText: {
-    color: 'white',
-    fontFamily: 'TeleGroteskNext-Regular',
-    fontSize: 20,
-    fontWeight: '400',
-  },
-  channelRow: {
-    flexDirection: 'row',
-    marginLeft: CHANNEL_NAME_WIDTH,
-    position: 'absolute',
-  },
-  channelName: {
-    width: CHANNEL_NAME_WIDTH,
-    height: ROW_HEIGHT,
-    backgroundColor: 'rgba(35,35,35, 0.9)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logo: {
-    width: 35,
-    height: 35,
-    resizeMode: 'contain',
-  },
-  rowLinesWrapper: {
-    top: ROW_HEIGHT,
-    width: '100%',
-    position: 'absolute',
-    overflow: 'hidden',
-  },
-  rowLinesContainer: {
-    position: 'absolute',
-    flexDirection: 'column',
-    width: '100%',
-  },
-  rowLines: {
-    width: '100%',
-    height: ROW_HEIGHT,
-    borderBottomWidth: 1,
-    borderColor: 'rgba(255,255,255,0.2)',
-  },
-  programRow: {
-    flexDirection: 'row',
-  },
-  program: {
-    justifyContent: 'center',
-    borderRightWidth: 1,
-    borderColor: 'rgba(255,255,255,0.2)',
-    paddingHorizontal: 10,
-    overflow: 'hidden',
-  },
-  programTextTitle: {
-    fontFamily: 'TeleGroteskNext-Medium',
-    fontSize: 22,
-    color: 'white',
-  },
-  programTextTitleRow: {
-    fontFamily: 'TeleGroteskNext-Regular',
-    flexDirection: 'row',
-  },
-  programText: {
-    color: 'white',
-    fontFamily: 'TeleGroteskNext-Regular',
-    fontSize: 16,
-  },
-  icon: {
-    top: 5,
-    marginLeft: 5,
-    width: 20,
-    height: 20,
-  },
-});
 
 export default EPG;
